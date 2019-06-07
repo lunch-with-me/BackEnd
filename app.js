f51a828c9a86b850b21ea4ee02896568172aeb66
Niroshan07 <Niroshan07@bitbucket.com>
2019-06-07 18:08:32 +0530

    auth finalized

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var db = require('./db');
+global.__root   = __dirname + '/'; 
+
+// add cors 
+var cors = require('cors');
+app.use(cors({
+  origin: '*',
+  credentials:true
+}));
+
+app.get('/api', function (req, res) {
+  res.status(200).send('API works.');
+});
+
+var UserController = require(__root + 'user/UserController');
+app.use('/api/users', UserController);
+
+var AuthController = require(__root + 'auth/AuthController');
+app.use('/api/auth', AuthController);
+
+module.exports = app;
---
+++ user/User.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');  
+var UserSchema = new mongoose.Schema({  
+  name: String,
+  email: String,
+  password: String,  
+  role: String,
+  username: String,
+  fullname: String,
+  gender: String,
+  date_of_birth:Date,
+  message:String,
+  telephone:String,
+  profession: String,
+  image: String,
+  creation_dt:Date
+});
+mongoose.model('User', UserSchema);
+
+module.exports = mongoose.model('User'); 
